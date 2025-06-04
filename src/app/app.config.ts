991e68ee4ff23e4e810c8844d13813d6f91f7286
abdelrahman <abdelrahman.elsheribini@gmail.com>
2025-06-04 23:44:32 +0300

    openapi

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {BASE_PATH} from './modules/openapi';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    { provide: BASE_PATH, useValue: 'http://localhost:8080' }
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  AdminProductsService,
+  AuthenticationService,
+  CustomerCategoriesService,
+  CustomerProductsService
+} from './modules/openapi';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +13,55 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'front';
+  constructor(private prod:AdminProductsService,private auth:AuthenticationService,private productService:CustomerProductsService,private categoriesService:CustomerCategoriesService) {}
+
+  ngOnInit() {
+    this.auth.login({
+      email: 'user2@example.com',
+      password: 'pass'
+    }).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+      },
+      error: (err) => {
+        console.error('Login failed:', err);
+      }
+    });
+    this.prod.deleteProduct(599).subscribe({
+      next: (response) => {
+        console.log('Product deleted successfully:', response);
+      },
+      error: (err) => {
+        console.error('Error deleting product:', err);
+      }
+    });
+
+    this.productService.getProducts1().subscribe({
+      next: (products) => {
+        console.log('Products loaded:', products);
+      },
+      error: (err) => {
+        console.error('Error loading products:', err);
+      }
+    });
+    this.categoriesService.getCategories1().subscribe({
+      next: (categories) => {
+        console.log('Categories loaded:', categories);
+      },
+      error: (err) => {
+        console.error('Error loading categories:', err);
+      }
+    })
+    this.productService.getProductById1(1).subscribe({
+      next: (product) => {
+        console.log('Product loaded:', product);
+      },
+      error: (err) => {
+        console.error('Error loading product:', err);
+      }
+    })
+
+  }
 }
