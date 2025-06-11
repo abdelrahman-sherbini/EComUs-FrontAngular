8249d4ba5ebc57076b859faf25e301f96b299103
abdelrahman <abdelrahman.elsheribini@gmail.com>
2025-06-11 05:53:58 +0300

    access token expire in 10 mins

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
-import {BASE_PATH} from './modules/openapi';
+import {BASE_PATH, Configuration} from './modules/openapi';
 import {AuthInterceptor} from './interceptors/auth-interceptor';
 import {environment} from '../environments/environment';
 
@@ -15,5 +15,12 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     { provide: BASE_PATH, useValue: `${environment.apiUrl}` },
     {provide:HTTP_INTERCEPTORS, useClass: AuthInterceptor,multi: true},
+    {
+      provide: Configuration,
+      useValue: new Configuration({
+        basePath: `${environment.apiUrl}`,
+        withCredentials: true
+      })
+    }
   ]
 };
